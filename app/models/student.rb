4938a3975f980b0ab86b5b5761186197aa5d1e4c
github username <github email address>
2017-03-03 12:02:38 -0500

    cool

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,15 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    if query.empty?
+      self.all
+    elsif self.where("name like ?", "%#{query}%").empty?
+      self.all
+    else
+      self.where("name like ?", "%#{query}%")
+    end
+
+  end
 end
